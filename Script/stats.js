2bbec64fe6cd3211be5cc455a0ef3baa1ebe6b1e
Fifa <132676120+77Henvi@users.noreply.github.com>
2026-05-31 22:07:12 +0700

    Update stats.js [F1lthy]

--- Script/stats.js
+++ Script/stats.js
@@ -1,7 +1,7 @@
 // ===== STATS =====
 // depends on: data, fmtDate, fmtMoney, emptyState, openModal
 
-import { fmtDate, fmtMoney, emptyState } from "./utils.js";
+import { fmtDate, fmtMoney, emptyState } from "./main.js";
 import { openModal } from "./ui.js";
 import { data } from "./main.js";
 
