85919f5c20f4e469395b29abfb89dbf935618d74
Fifa <132676120+77Henvi@users.noreply.github.com>
2026-06-30 19:31:43 +0700

    Update app.js [F1lthy]
    -- johndoe with the push

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { addDoc, deleteDoc, doc, updateDoc, onSnapshot } from "https://www.gstat
 import { auth, db, colStock, colFinance, colOrders, colTodos } from "./firebase-config.js";
 import { todayStr, thisMonthStr, fmtDate, fmtMoney, emptyState } from "./utils.js";
 import "./auth.js"; 
-import "./catalog-admin.js";
+import "./catalog-public.js";
 
 // ===== DATA STATE =====
 let data = { stock: [], finance: [], orders: [], todos: [] };
